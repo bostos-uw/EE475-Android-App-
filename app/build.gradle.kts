e4d9b8053f1506ced49bc6f0d2f9f7bfa08dd9cb
Boston Susanto <bostos@uw.edu>
2025-10-25 22:38:24 -0700

    Debugged Navigation Bar, Added Layout for Home Fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.12.0")
+    // already likely present
+    implementation("androidx.navigation:navigation-fragment:2.8.3")
+    implementation("androidx.navigation:navigation-ui:2.8.3")
 }
